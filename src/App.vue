15d5005c93568d4cd1a6410f2a1f00ae2383cedd
= <=>
2022-05-11 17:58:18 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div
+    class="
+      w-screen
+      h-screen
+      bg-gray-800
+      flex flex-row
+      text-white
+      items-start
+      justify-center
+    "
+  >
+    <app-sidebar :invoices="invoiceList" :editInvoice="editInvoice" />
+    <invoice-content :saveInvoice="saveInvoice" :activeInvoice = "state.activeInvoice" />
+  </div>
+</template>
+<script setup>
+import { ref,reactive } from "vue";
+import appSidebar from "./components/appSidebar.vue";
+import invoiceContent from "./components/invoiceContent.vue";
+const invoiceList = ref([
+  {
+    contact: {
+      contact_name: "demo 1",
+      email: "demo@demo.com",
+      city: "Demo City",
+      country: "Demo country",
+      zipcode: "demozipcode",
+    },
+    items: [
+      {
+        id: new Date().getTime(),
+        name: "demo ürün",
+        qty: 1,
+        unit_price: 10.0,
+        total_price: 10.0,
+      },
+    ],
+  },
+]);
+
+const state = reactive({activeInvoice:null});
+const saveInvoice = (invoice) => {
+  invoiceList.value.push(invoice);
+};
+
+const editInvoice = (invoice) => {
+  state.activeInvoice = invoice;
+};
+</script>
